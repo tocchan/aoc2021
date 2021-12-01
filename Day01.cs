099b75ff0dbe89f0b721fac05910291d39b791eb
Chris Forseth (C4) <cforseth@straykitestudios.com>
2021-12-01 07:21:29 -0600

    Day 01 : Adventures of LINQ

--- Day01.cs
+++ Day01.cs
@@ -8,21 +8,47 @@ namespace AoC2021
 {
     internal class Day01
     {
-        private string InputFileA = "inputs/test.txt"; 
-        private string InputFileB = "inputs/text.txt"; 
+        private string InputFileA = "inputs/day01.a.txt"; 
+        private string InputFileB = "inputs/day01.a.txt"; 
 
         //----------------------------------------------------------------------------------------------
         public string RunA()
         {
             List<string> lines = Util.ReadFileToLines(InputFileA); 
-            return (lines.Count > 0) ? lines[0] : "empty"; 
+            List<int> values = lines.Select(int.Parse).ToList(); 
+
+            int count = 0; 
+            for (int i = 1; i < values.Count; ++i)
+            {
+                if (values[i] > values[i - 1])
+                {
+                    ++count; 
+                }
+            }
+
+            return count.ToString(); 
         }
 
         //----------------------------------------------------------------------------------------------
         public string RunB()
         {
-            List<string> lines = Util.ReadFileToLines(InputFileA); 
-            return (lines.Count > 0) ? lines[0] : "empty"; 
+            List<string> lines = Util.ReadFileToLines(InputFileB); 
+            List<int> values = lines.Select(int.Parse).ToList(); 
+
+            int count = 0; 
+            int prevSum = values[0] + values[1] + values[2]; 
+
+            for (int i = 3; i < values.Count; ++i)
+            {
+                int sum = values[i] + values[i - 1] + values[i - 2]; 
+                if (sum > prevSum)
+                {
+                    ++count; 
+                }
+                prevSum = sum;
+            }
+
+            return count.ToString(); 
         }
     }
 }
