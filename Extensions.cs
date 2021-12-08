bbd232da30ff144d03eea3bb7eae7ee6885c0310
Chris Forseth (C4) <cforseth@straykitestudios.com>
2021-12-08 11:44:08 -0600

    Turn nullability back on, and return default-bang!

--- Extensions.cs
+++ Extensions.cs
@@ -10,14 +10,14 @@ namespace AoC2021
     {
         public static void Deconstruct<T>(this IList<T> list, out T first, out T second) 
         {
-            first = list.Count > 0 ? default : default; 
-            second = list.Count > 1 ? list[1] : default; 
+            first = list.Count > 0 ? list[0] : default!;
+            second = list.Count > 1 ? list[1] : default!; 
         }
 
         public static void Deconstruct<T>(this IList<T> list, out T first, out T second, out IList<T> rest) 
         {
-            first = list.Count > 0 ? list[0] : default; // or throw
-            second = list.Count > 1 ? list[1] : default; // or throw
+            first = list.Count > 0 ? list[0] : default!; 
+            second = list.Count > 1 ? list[1] : default!;
             rest = list.Skip(2).ToList();
         }
     }
