f0f15cb933cb461766193d3ba1617575bc945cfd
Chris Forseth (C4) <cforseth@straykitestudios.com>
2021-12-20 14:52:15 -0600

    Day20 prep and inputs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using AoC2021;
 
-Day? day = Day.Create( args.FirstOrDefault(), typeof(Day19) ); 
+Day? day = Day.Create( args.FirstOrDefault(), typeof(Day20) ); 
 
 Util.WriteLine( @"[  cyan]         .--._.--.--.__.--.--.__.--.--.__.--.--._.--.     "); 
 Util.WriteLine( @"[  cyan]       _(_      _Y_      _Y_      _Y_      _Y_      _)_   "); 
